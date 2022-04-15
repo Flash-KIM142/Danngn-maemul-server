d0b8208a7738fdb00554e249db080babd1a0d9fd
Patrick Ryoo <donghoon149@gmail.com>
2022-04-15 16:49:04 +0900

    initial setting

---
+++ src/main/java/com/mamul/api/MamulServerApplication.java
@@ -0,0 +1,13 @@
+package com.mamul.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MamulServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MamulServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mamul/api/MamulServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mamul.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MamulServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
